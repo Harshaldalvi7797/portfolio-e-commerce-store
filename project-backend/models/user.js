055db7ee230ee471b501055ce4535aaae8dd0404
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-12-21 15:56:14 +0530

    commit : category schema

--- project-backend/models/user.js
+++ project-backend/models/user.js
@@ -1,6 +1,8 @@
 /** @format */
 
 import mongoose from "mongoose";
+const crypto = require("crypto");
+const uuidv1 = require("uuidv1/v1");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,7 +27,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  password: {
+  encry_password: {
     type: String,
     required: true
   },
@@ -39,5 +41,33 @@ const userSchema = new mongoose.Schema({
     default: []
   }
 });
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.method = {
+  autheticate: function(plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
+
+  securePassword: function(plainpassword) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  }
+};
 
 module.exports = mongoose.model("User", userSchema);
