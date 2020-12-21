faaeb01d290d837387c334b601e8638828da98f1
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-12-21 21:09:58 +0530

    commit : enviroment variables using dotenv

--- project-backend/app.js
+++ project-backend/app.js
@@ -1,3 +1,23 @@
 /** @format */
-
+require("dotenv").config();
 const express = require("express");
+const app = express();
+// getting-started.js
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const port = 8000;
+app.listen(() => {
+  console.log(`app is running ${port}`);
+});
