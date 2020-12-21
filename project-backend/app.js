a0676880199506989ac44e45d44c4ddf5044a5ad
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-12-22 00:37:30 +0530

    commit : api call

--- project-backend/app.js
+++ project-backend/app.js
@@ -5,6 +5,8 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
+const authRoutes = require("./routes/auth");
 // getting-started.js
 const mongoose = require("mongoose");
 mongoose
@@ -20,11 +22,17 @@ mongoose
     console.log(err);
   });
 
+//Middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
-const port = 8000;
-app.listen(() => {
+//My Routes
+app.use("/api", authRoutes);
+
+//Port
+const port = process.env.PORT || 5000;
+//starting server
+app.listen(port, () => {
   console.log(`app is running ${port}`);
 });
