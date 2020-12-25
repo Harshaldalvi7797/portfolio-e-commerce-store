0f36a8f1d94a71a8aafde10c82e75bc081f1b92a
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-12-25 23:31:04 +0530

    commit: express validator

--- project-backend/controller/auth.js
+++ project-backend/controller/auth.js
@@ -1,13 +1,18 @@
 /** @format */
 
 const express = require("express");
-const router = express.Router();c
+const router = express.Router();
 const User = require("../models/user");
+const { validationResult } = require("express-validator");
 // router.get("/signout", (req, res) => {
 //   res.send("user signout");
 // });
 exports.signout = (req, res) => {};
 exports.signup = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ error: errors.array()[0].msg });
+  }
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
--- project-backend/routes/auth.js
+++ project-backend/routes/auth.js
@@ -3,9 +3,15 @@
 var express = require("express");
 var router = express.Router();
 
-const { signup } = require("../controller/auth");
-router.post("/signup", signup);
+const { body, check } = require("express-validator");
 
-const { signout } = require("../controller/auth");
+const { signup, signout } = require("../controller/auth");
+router.post(
+  "/signup",
+  [check("name", "must be at least 5 chars long").isLength({ min: 5 })],
+  signup
+);
+
+// router.post("/signup", signup);
 router.get("/signout", signout);
 module.exports = router;
