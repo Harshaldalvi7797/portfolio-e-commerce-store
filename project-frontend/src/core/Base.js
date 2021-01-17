f80977ec0d7ddc772cd19a8b574881909ecce3fe
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2021-01-17 17:17:57 +0530

    commit : signup successfully front to database

--- project-frontend/src/core/Base.js
+++ project-frontend/src/core/Base.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Menu from "./Menu"
+import "../style.css"
 const Base = ({ title = "My Title",
     description = "My desription",
-    className = "bg-dark text-white p-4",
+    className = "bg-dark text-white",
     children }) => {
     return (
         <div>
