268cc2b8fec90704a7fa9b79a9a116d1ed5f168a
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2021-01-18 21:19:23 +0530

    commit : admin dashboard static

--- project-frontend/src/core/Menu.js
+++ project-frontend/src/core/Menu.js
@@ -27,24 +27,33 @@ const Menu = ({ history }) => (
                     Cart
         </Link>
             </li>
-            <li className="nav-item">
-                <Link
-                    style={currentTab(history, "/user/dashboard")}
-                    className="nav-link"
-                    to="/user/dashboard"
-                >
-                    Dashboard
-        </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    style={currentTab(history, "/admin/dashboard")}
-                    className="nav-link"
-                    to="/admin/dashboard"
-                >
-                    A. Dashboard
-        </Link>
-            </li>
+            {isAutheticated() && isAutheticated().user.role === 0 && (
+                <li className="nav-item">
+                    <Link
+                        style={currentTab(history, "/user/dashboard")}
+                        className="nav-link"
+                        to="/user/dashboard"
+                    >
+                        U. Dashboard
+          </Link>
+                </li>
+            )}
+            {
+                isAutheticated() && isAutheticated().user.role === 1 &&
+                (
+                    <li className="nav-item">
+                        <Link
+                            style={currentTab(history, "/admin/dashboard")}
+                            className="nav-link"
+                            to="/admin/dashboard"
+                        >
+                            A. Dashboard
+            </Link>
+                    </li>
+
+                )
+            }
+
             {
                 !isAutheticated() && (
                     <Fragment>
--- project-frontend/src/user/Profile.js
+++ project-frontend/src/user/Profile.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 import Base from "./../core/Base";
-export default function Profile() {
+const Profile = () => {
     return (
         <Base title="Profile Page">
             <h1>This is the profile page</h1>
         </Base>
     )
 }
+export default Profile
