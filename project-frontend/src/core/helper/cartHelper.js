cf05e60e930c2864608729f60573af3b804a95fd
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2021-01-22 00:20:19 +0530

    commit : order helper post api

--- project-frontend/src/core/helper/cartHelper.js
+++ project-frontend/src/core/helper/cartHelper.js
@@ -41,3 +41,9 @@ export const removeItemFromCart = (productId) => {
     }
     return cart;
 }
+export const cartEmpty = next => {
+    if (typeof window !== undefined) {
+        localStorage.removeItem("cart");
+        next();
+    }
+};
