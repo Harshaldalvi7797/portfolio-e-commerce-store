f8902130bcbc0f1984c072bb01ee6e38178f3138
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2021-01-22 00:00:08 +0530

    commit : addcart and removecart successfull

--- project-frontend/src/core/Card.js
+++ project-frontend/src/core/Card.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect } from "react";
 import ImageHelper from "./helper/ImageHelper";
 import { Redirect } from "react-router-dom";
-// import { addItemToCart } from "./helper/cartHelper";
+import { addItemToCart, removeItemFromCart } from "./helper/cartHelper";
 
-const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
+const Card = ({ product, addtoCart = true, removeFromCart = false,
+    setReload = f => f, reload = undefined}) => {
     const [redirect, setRedirect] = useState(false);
-    // const [count, setCount] = useState(product.count);
+    const [count, setCount] = useState(product.count);
 
     const cartTitle = product ? product.name : "A photo from pexels";
     const cartDescrption = product ? product.description : "Default description";
     const cartPrice = product ? product.price : "DEFAULT";
 
-    // const addToCart = () => {
-    //     addItemToCart(product, () => setRedirect(true));
-    // };
+    const addToCart = () => {
+        addItemToCart(product, () => setRedirect(true));
+    };
 
-    // const getARedirect = redirect => {
-    //     if (redirect) {
-    //         return <Redirect to="/cart" />;
-    //     }
-    // };
+    const getARedirect = redirect => {
+        if (redirect) {
+            return <Redirect to="/cart" />;
+        }
+    };
 
     const showAddToCart = addtoCart => {
         return (
             addtoCart && (
                 <button
-
+                    onClick={addToCart}
                     className="btn btn-block btn-outline-success mt-2 mb-2"
                 >
                     Add to Cart
@@ -38,7 +39,7 @@ const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
         return (
             removeFromCart && (
                 <button
-                    onClick={() => { }}
+                    onClick={() => { removeItemFromCart(product._id); setReload(!reload) }}
                     className="btn btn-block btn-outline-danger mt-2 mb-2"
                 >
                     Remove from cart
@@ -50,7 +51,7 @@ const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
         <div className="card text-white bg-dark border border-info ">
             <div className="card-header lead">{cartTitle}</div>
             <div className="card-body">
-
+                {getARedirect(redirect)}
                 <ImageHelper product={product} />
                 <p className="lead bg-success font-weight-normal text-wrap">
                     {cartDescrption}
