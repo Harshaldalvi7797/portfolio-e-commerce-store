cc9eab7610a02aaa24c53f83c1157ece360fe4f9
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2021-01-23 00:36:01 +0530

    commit : stripecheckout add in frontend

---
+++ project-frontend/src/core/StripeCheckout.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import { isAutheticated } from "./../auth/helper/index";
+import { Link } from "react-router-dom"
+import StripeCheckoutButton from "react-stripe-checkout";
+const StripeCheckout = ({ products, setReload = f => f, reload = undefined }) => {
+    const [data, setData] = useState({
+        loading: false,
+        success: false,
+        error: "",
+        address: ""
+    });
+
+    const token = isAutheticated() && isAutheticated().token;
+    const userId = isAutheticated() && isAutheticated().user._id;
+
+    const getFinalAmount = () => {
+
+        let amount = 0;
+        products.map(p => {
+            amount = amount + p.price;
+        });
+        return amount;
+
+    }
+    const makePayment = token => {
+        //
+    };
+    console.log(products.price)
+    const showStripeButton = () => {
+        return isAutheticated() ? (
+            <StripeCheckoutButton stripeKey="" token="" amount={getFinalAmount() * 100}
+                name="Buy Ti-Shirts"
+                shippingAddress
+                billingAddress
+
+            >
+                <button className="btn btn-success">Pay with stripe</button></StripeCheckoutButton>
+        ) : (
+                <Link to="/signin">
+                    <button className="btn btn-warning">Signin</button>
+                </Link>
+            );
+    };
+    return (
+        <div>
+            <h3 className="text-white">Stripe Checkout {getFinalAmount()}</h3>
+            {showStripeButton()}
+
+        </div>
+    )
+}
+export default StripeCheckout
